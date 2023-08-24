f3ca515bdbc738529eca4931a794d9776325bc4f
James Byrne <jambyr@bas.ac.uk>
2023-08-24 11:33:02 +0100

    Dev #3: API endpoint for interactive plotting

--- icenet_app/icenet.py
+++ icenet_app/icenet.py
@@ -1,9 +1,32 @@
 import datetime as dt
+import logging
+import numpy as np
+import os
 
 import pandas as pd
+import xarray as xr
+
 from flask import jsonify, send_file
 
 from icenet_app.utils import get_forecast_data
+
+
+def get_image_data(forecast_date: str,
+                   leadtime: int = 1,
+                   data_type: str = "sic_mean",
+                   region: tuple = (110, 235, 140, 265)):
+    icenet_data = os.environ["ICENET_DATA_LOCATION"] \
+        if "ICENET_DATA_LOCATION" in os.environ else os.path.join(".", "data")
+
+    ds = xr.open_dataset(os.path.join(icenet_data, "north_daily_forecast.{}.nc".format(forecast_date)))
+
+    da = ds.isel(time=0, leadtime=int(leadtime))
+    pred_data = getattr(da, data_type).to_numpy()
+
+    x1, y1, x2, y2 = region
+    pred_data = pred_data[(len(ds.yc) - y2):(len(ds.yc) - y1), x1:x2]
+
+    return jsonify(forecast_data=pred_data.tolist())
 
 
 def get_image(forecast_date: str,
